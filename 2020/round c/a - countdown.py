eaa3dd2772e13733fa6ca7a805134175a12b4db2
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2020-11-19 12:28:24 -0500

    Added main code

--- 2020/round c/a - countdown.py
+++ 2020/round c/a - countdown.py
@@ -1,5 +1,19 @@
 caseCount = int(input())
 for i in range(1, caseCount + 1):
-    intCount, limit = [int(s) for s in input().split(" ")]
+    intCount, goal = [int(s) for s in input().split(" ")]
     total = 0
+    numbers = list(map(int, input().split()))
+    if len(numbers) == intCount:
+        for k in numbers:
+            if numbers[k-1] == goal:
+                loopCount = 1
+                mode = True
+                while goal - loopCount > 0:
+                    if numbers[(k-1)+loopCount] - numbers[k+loopCount] == 1:
+                        loopCount += 1
+                    else:
+                        mode = False
+                        break
+                if mode:
+                    total += 1
     print("Case #{}: {}".format(i, total))
