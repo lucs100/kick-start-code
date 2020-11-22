7f06559dda0e23755ca5c01c7aee5ba72b6fb363
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2020-11-21 23:13:09 -0500

    Implemented placeholder code

--- 2020/round e/a - longest arithmetic.py
+++ 2020/round e/a - longest arithmetic.py
@@ -1,8 +1,7 @@
-def 
-
 caseCount = int(input())
 for i in range(1, caseCount + 1):
     numCount = [int(s) for s in input().split(" ")]
     x = list(map(int, input().split()))
+    result = 0
     
     print("Case #{}: {}".format(i, result))
