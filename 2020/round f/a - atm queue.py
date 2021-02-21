e92614a249569cb4a3adc2dfe289bfc33ddfbb6e
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-02-20 23:35:21 -0500

    Refactored solution. Scored 5/13.

--- 2020/round f/a - atm queue.py
+++ 2020/round f/a - atm queue.py
@@ -1,23 +1,22 @@
 caseCount = int(input())
 for i in range(1, caseCount + 1):
     countPeople, maxWithdraw = [int(s) for s in input().split(" ")]
-    peopleList = list(map(int, input().split()))
-    orderList = list(range(countPeople))
-    for i in orderList:
-        orderList[i] += 1
-    finalList = []
-    while len(peopleList) != 0:
-        peopleList[0] -= maxWithdraw
-        if peopleList[0] <= 0:
-            finalList.append(orderList[0])
-            orderList.pop(0)
-            peopleList.pop(0)
+    line = []
+    inputs = [int(s) for s in input().split(" ")]
+    for j in range(len(inputs)):
+        line.append([j+1, inputs[j]])
+    exitOrder = []
+    while len(line) != 0:
+        cash = line[0][1]
+        cash -= maxWithdraw
+        if cash > 0:
+            line.append([line[0][0], cash])
+            line.pop(0)
         else:
-            orderList.append(orderList[0])
-            orderList.pop(0)
-            peopleList.append(peopleList[0])
-            peopleList.pop(0)
-    exitOrder = str(finalList).strip("[]")
-    print(exitOrder + "done")
-    exitOrder = exitOrder.replace(', ', ' ')
-    print("Case #{}: {}".format(i, exitOrder))
+            exitOrder.append(line[0][0])
+            line.pop(0)
+    exitStr = ""
+    for j in range(countPeople):
+        exitStr += str(exitOrder[j])
+        exitStr += " "
+    print("Case #{}: {}".format(i, exitStr[:-1]))
