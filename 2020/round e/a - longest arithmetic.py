a73e175916cbc5371d778a1d3ca7f2a449dab533
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2021-06-03 00:34:03 -0400

    New file

--- 2020/round e/a - longest arithmetic.py
+++ 2020/round e/a - longest arithmetic.py
@@ -1,7 +1,23 @@
+# Scores 0/11 (4, 7) points
+
+import math
+
 caseCount = int(input())
 for i in range(1, caseCount + 1):
     numCount = [int(s) for s in input().split(" ")]
-    x = list(map(int, input().split()))
-    result = 0
-    
-    print("Case #{}: {}".format(i, result))
+    numbers = list(map(int, input().split()))
+    longest = 0
+    streak = 1
+    last = float('nan')
+    for j in range(1, len(numbers)):
+        current = (numbers[j-1] - numbers[j])
+        if current == last:
+            streak += 1
+        elif math.isnan(last):
+            streak == 2
+        else:
+            streak == 1
+
+        longest = max(longest, streak)
+        last = current
+    print("Case #{}: {}".format(i, longest))
